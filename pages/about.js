0075ed764fd2d35b3c19cc0b10471b08614323dc
Abhishek2262 <abhishekpandey1480@gmail.com>
2022-07-22 14:58:48 +0530

    boom

--- pages/about.js
+++ pages/about.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 import Card from "../components/card";
 import logo from "../public/rotaractLogo.svg";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
-import { FaFacebook,FaInstagram,FaRegEnvelope,FaPhoneAlt,FaLocationArrow,FaLinkedinIn } from "react-icons/fa";
-
+import { FaFacebook,FaInstagram,FaRegEnvelope,FaPhoneAlt,FaLocationArrow,FaLinkedinIn,FaArrowCircleLeft,FaArrowCircleRight } from "react-icons/fa";
 import { useEffect } from "react";
+
 
 export default function About(props) {
   useEffect(() => {
@@ -17,21 +17,16 @@ export default function About(props) {
 
   return (
     <>
-      <div className="bg-slate-100  text-slate-900 sm:min-h-[19rem] sm:p-4 border-1  ">
+      <div className="bg-slate-100  text-slate-900 sm:min-h-[19rem] sm:p-4 border-1 ">
         <h1
-          className="rtr1 sm:text-center md:text-center {hover:tracking-widest duration-300}"
+          className="rtr1 text-center  {hover:tracking-widest duration-300}"
           data-aos="fade-up"
         >
-          {" "}
           Rotary
         </h1>
 
         <div className=" sm:m-5 ">
-          <div
-            className="  bg-slate-100 sm:min-h-[12rem] sm:min-w-[10rem] sm:flex  items-center sm:text-clip "
-            data-aos="fade-up"
-          >
-            <p className="sm:max-h-[8rem] text-center text-sm sm:text-lg md:text-lg">
+        <p className=" sm:min-h-[12rem] sm:min-w-[10rem] p-3 text-center text-sm sm:text-lg md:text-lg"  data-aos="fade-up">
               Rotaract clubs bring together people ages 18 and older to exchange
               ideas with leaders in the community, develop leadership and
               professional skills, and have fun through service.In communities
@@ -39,13 +34,13 @@ export default function About(props) {
               action through service. From big cities to rural villages,
               Rotaract is changing communities like yours.
             </p>
-          </div>
+         
         </div>
       </div>
 
-      <div className="abt-1  bg-slate-100 sm:min-h-[25rem] rounded-lg border-1 P-1">
-        <h2 className=" text-center sm:text-xlg ">About</h2>
-        <p data-aos="fade-up" className=" text-center p-2 text-sm sm:text-lg">
+      <div className="abt-1  bg-slate-100 sm:min-h-[25rem] rounded-lg border-1 P-1 bg-gradient-to-r from-pink-500 to-purple-500 ">
+        <h2 className=" text-center sm:text-xlg text-white">About</h2>
+        <p data-aos="fade-up" className=" text-white text-center p-2 text-sm sm:text-lg bg-gradient-to-r from-pink-500 to-purple-500 ">
           {" "}
           Rotaract originally began as a Rotary International youth program in
           1968 at Charlotte North Rotary Club in Charlotte, North Carolina,
@@ -63,13 +58,18 @@ export default function About(props) {
           created by Rotary International in 1962.
         </p>
       </div>
+      
 
-      <div className="bg-slate-100  overflow-scroll snap-x snap-mandatory sm:flex gap-40 p-8 px-36 place-items-center w-full">
-        <div className="flex justify-center gap-32 snap-center ">
+      <div className="bg-white  overflow-scroll  snap-x sm:flex gap-40 p-8 px-36 place-items-center w-full" data-bs-ride="carousel">
+                 
+        
+        <div className="flex justify-center gap-32 snap-center  " >
+        
           <Card name="Aryan Raj" post="President" img={props.foo} />
           <Card name="Souvik Dey" post={"Joint-President"} />
           <Card name="Abhishek Pandey" post={"Vice-President"} />
           <Card name="Sakshi" post={"Vice-President"} />
+          
         </div>
 
         <div className="flex justify-center gap-32 snap-center ">
@@ -88,29 +88,27 @@ export default function About(props) {
       </div>
 
       <div
-        className="intro bg-slate-100 text-center min-h-[35rem] p-8"
+        className="intro bg-slate-100 text-center bg-gradient-to-r from-pink-500 to-purple-500 min-h-[35rem] p-8"
         data-aos="fade-up"
       >
         {" "}
-        <h2 className=""> Introduction </h2>
+        <h2 className="text-white"> Introduction </h2>
       </div>
 
-      <div>
+      <div className=""data-aos="fade-up">
         <div className="contact bg-white sm:min-h-[30rem] text-pink-500 p-10 gap-5 ">
-          <h2>GET IN TOUCH</h2>{" "}
-          <div className=" sm:float-left sm:text-left sm:min-h-[5rem] text-sm sm:text-md pl-2 sm:w-[25rem] text-slate-800">
+          <h2 className="" data-aos="fade-right">GET IN TOUCH</h2>{" "}
+          <div data-aos="fade-right" className=" sm:float-left sm:text-left sm:min-h-[5rem] text-sm sm:text-md pl-2 sm:w-[25rem] text-slate-800">
             {" "}
             fyfi doie fgefwe fgfewfgw igf wiufg f uvb vod ud dktou cvsui csdg
             cvsdu f hifh ioh h ohgsfggogfsogf ufdg f ooghhvhidhvvihh
-            <div
+            <div 
               style={{
                
                 paddingLeft: "24px",
                 paddingTop: "24px",
                 display: "flex",
               }}
-
-
 
               
             >
@@ -160,44 +158,44 @@ export default function About(props) {
 
 
           </div>
-          <form className="review  bg-white sm:min-w-[28rem] sm:float-right sm:min-h-[27rem] space-y-7 drop-shadow-2xl  pr-8 pl-8 ">
+          <form data-aos="fade-down" className="review  bg-white min-w-[28rem] float-  min-h-[15rem] sm:float-right sm:min-h-[27rem]  space-y-7 drop-shadow-2xl  pr-8 pl-8 ">
             <h2 className=" sm:text-center text-pink-500">Write a Review</h2>
             <input
               className="palceholder:text-slate-800 border-2 border-pink-500 sm:w-[100%] text-sm pt-1 pb-1 pl-1 "
               placeholder="Name"
             />
-            <div className="email border-2  sm:float-center border-pink-500">
+            <div className="email  sm:float-center border-pink-500">
               <input
-                className=" sm:w-[100%] text-sm  pt-1 pb-1 pl-1 border-pink-500"
+                className="text-sm w-[100%] pt-1 pb-1 pl-1 border-2 border-pink-500 "
                 placeholder="Email"
               ></input>
             </div>
             <textarea
-              className="sm:w-[100%] border-2 border-pink-500 text-sm  pt-1 pb-1 pl-1"
+              className="sm:w-[100%] border-2 border-pink-500 text-sm  pt-1 pb-1 pl-1 resize"
               placeholder="Write a reviews"
               rows={4}
             />
-            <div className="sm:flex justify-center">
-              <button className="sm:w-[70%] border-2 sm:h-[3rem] bg-pink-500 text-slate-100">
+            <div className="sm:flex justify-center ">
+              <button className="sm:w-[70%] border-2  hover:text-slate-800 hover:cursor-pointer sm:h-[3rem] bg-pink-500 text-slate-100">
                 Button
               </button>
             </div>
           </form>
         </div>
         <footer className="foorter sm:bg-pink-500 p-10 sm:min-h-[15rem]">
-            <h5 className="text-white w-[20rem] text-center"> Rotaract Club Of BIT Mesra</h5>
-            <div className="text-sm text-white w-[17rem] text-left pl-3">Abh igwh csc yhucs klkajshbfgfsvbc ttdsvcsatfsa sasathgvcsatxsavc cshgsacvschsafsafcas uycfsacsacsafs vcsufccsacfucuyc</div>
+            <h5 className="text-white w-[20rem] text-center hover:text-slate-800 hover:cursor-pointer"> Rotaract Club Of BIT Mesra</h5>
+            <div className="text-sm text-white w-[17rem] text-left pl-3 hover:text-slate-800 hover:cursor-pointer">Abh igwh csc yhucs klkajshbfgfsvbc ttdsvcsatfsa sasathgvcsatxsavc cshgsacvschsafsafcas uycfsacsacsafs vcsufccsacfucuyc</div>
 
-            <div className="flex flex-row gap-3 p-3 w-[8rem]">
-            <FaFacebook/>
-            <FaInstagram/>
-            <FaLinkedinIn/> 
+            <div className="flex flex-row gap-3 p-3 w-[8rem] text-white ">
+            <FaFacebook className="hover:text-slate-800 hover:cursor-pointer"/>
+            <FaInstagram className="hover:text-slate-800 hover:cursor-pointer"/>
+            <FaLinkedinIn className="hover:text-slate-800 hover:cursor-pointer"/> 
             </div>
             <div className="flex justify-center gap-20  ">
-                <div className="text-sm text-white">Home</div>
-                <div className="text-sm text-white">About</div>
-                <div className="text-sm text-white">Event</div>
-                <div className="text-sm text-white">Contact</div>
+                <div className="text-sm text-white hover:text-slate-800 hover:cursor-pointer">Home</div>
+                <div className="text-sm text-white hover:text-slate-800 hover:cursor-pointer">About</div>
+                <div className="text-sm text-white hover:text-slate-800 hover:cursor-pointer">Event</div>
+                <div className="text-sm text-white hover:text-slate-800 hover:cursor-pointer">Contact</div>
             </div>
             
         </footer>
