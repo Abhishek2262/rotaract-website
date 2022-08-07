e0ff2c455a4abf24d37643f0aef0530f699868f1
Abhishek2262 <abhishekpandey1480@gmail.com>
2022-08-07 10:26:34 +0530

    Event page

---
+++ pages/events.js
@@ -0,0 +1,15 @@
+export default function Events(){
+  return(
+    <>
+ 
+    <div className=" min-h-[53rem]  min-w-[30rem]  bg-[url('../public/_MG_0041.jpg')] bg-no-repeat bg-cover ">
+      <h2 className="text-white text-center">OUR EVENTS</h2>
+
+      <p className="text-center text-white p-5"> Hi ujgfvs dugob vsdvsdb ougvv osigv vsdvnivsd vdsnbvdsghidh vodgvsdihvsdhsdgvsd vsdkgvsdvb sddvoigvvhdvhds  gugdvbsds dg dsg  ihgvvb vvbnvsfbo hbif hbfhf  hghdf </p>
+
+    </div>
+    
+   
+    </>
+  )
+}
